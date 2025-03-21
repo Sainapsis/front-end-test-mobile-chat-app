57feac7d0506aa88467063b9a02457b063408d94
Luis Alfonso <luealfonsoru@gmail.com>
2025-03-21 03:46:09 -0500

    hotfix: added changes on localstrategy.ts

--- back-end-test-mobile-chat-app/src/api/auth/utils/localStrategy.ts
+++ back-end-test-mobile-chat-app/src/api/auth/utils/localStrategy.ts
@@ -5,19 +5,19 @@ import {
 
   Logger
 } from '@nestjs/common';
-// import { AuthService } from '../auth.service';
+import { AuthService } from '../auth.service';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
   logger: Logger;
-//   constructor(private authService: AuthService) {
-//     super({ usernameField: 'username' });
-//     this.logger = new Logger(LocalStrategy.name)
-//   }
+  constructor(private authService: AuthService) {
+    super({ usernameField: 'username' });
+    this.logger = new Logger(LocalStrategy.name)
+  }
 
-//   async validate(email: string, username: string): Promise<any> {
-//     const user = await this.authService.validateUser(email, username);
-//     this.logger.log('Logged In User ,checking at local auth:', user);
-//     return user;
-//   }
+  async validate(email: string, username: string): Promise<any> {
+    const user = await this.authService.validateUser(email, username);
+    this.logger.log('Logged In User ,checking at local auth:', user);
+    return user;
+  }
 }
